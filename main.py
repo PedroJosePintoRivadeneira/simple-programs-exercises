1c5a7eb62bc360e3316ebbb99ddac01d9c1da3f8
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-30 06:47:30 -0500

    What note do I need?

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-# Exercise 8. Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+#Exercise 9. Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo.
+#El promedio del ramo se calcula con la siguiente formula.
 
-#Ingrese un numero: 4.5
-#0.5
-#Ingrese un numero: -1.19
-#0.19
+#NC=(C1+C2+C3)3
+#NF=NC⋅0.7+NL⋅0.3
+#Donde NC
+# es el promedio de certámenes, NL
+# el promedio de laboratorio y NF
+# la nota final.
 
-number = float(input("Enter a number "))
+#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
 
-decimal_part = round(abs(number - int(number)), 3)
+#Ingrese nota certamen 1: 45
+#Ingrese nota certamen 2: 55
+#Ingrese nota laboratorio: 65
+#Necesita nota 72 en el certamen 3
 
-print(f"{decimal_part}")
+contest_1 = float(input("Enter contest note 1 "))
+contest_2 = float(input("Enter contest note 2 "))
+laboratory_note = float(input("Enter laboratory note "))
+
+desired_note = 60
+
+contest_3 = ((desired_note - laboratory_note * 0.3) / 0.7) * 3 - contest_1 - contest_2
+
+print(f"""
+Need note {round(contest_3, 1)} in the contest
+""")
