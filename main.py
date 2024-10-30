b9a027823b7e75a58b94a2e605e6f42402acdf84
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-30 06:20:45 -0500

    Greeting

---
+++ main.py
@@ -0,0 +1,11 @@
+#Exercise 1. Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+
+#Example: 
+#Ingrese su nombre: Perico
+#Hola, Perico
+
+#Solution 1
+
+name = input("Enter your name: ")
+
+print(f"Hi {name}")
