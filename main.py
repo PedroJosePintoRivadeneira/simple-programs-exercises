f808794606af13a68fc729a3500b36fefedbc34d
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-30 06:40:04 -0500

    Decimal Part

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-# Exercise 7. Escriba un programa que pregunte al usuario la hora actual t del reloj y un número entero de horas h, que indique qué hora marcará el reloj dentro de h horas:
+# Exercise 8. Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
 
-#Hora actual: 3
-#Cantidad de horas: 5
-#En 5 horas, el reloj marcara las 8
-#Hora actual: 11
-#Cantidad de horas: 43
-#En 43 horas, el reloj marcara las 6
+#Ingrese un numero: 4.5
+#0.5
+#Ingrese un numero: -1.19
+#0.19
 
-p = int(input("Current time "))
-h = int(input("Number of hours "))
-f = (p + h) % 24
+number = float(input("Enter a number "))
 
-if f == 0:
-    f = 24
+decimal_part = round(abs(number - int(number)), 3)
 
-print(f"""
-      In {h} hours, the clock will strike {f}
-""")
+print(f"{decimal_part}")
