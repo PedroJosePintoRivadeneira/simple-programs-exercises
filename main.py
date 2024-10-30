4a2d06a82f423c49d497ba3558f89ca82550f508
PedroJosePintoRivadeneira <186082557+PedroJosePintoRivadeneira@users.noreply.github.com>
2024-10-30 06:24:12 -0500

    Circles

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-#Exercise 1. Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+# Exercise 2. Escriba un programa que reciba como entrada el radio de un círculo y entregue como salida su perímetro y su área:
 
-#Example: 
-#Ingrese su nombre: Perico
-#Hola, Perico
+#Example:
+#Ingrese el radio: 5
+#Perimetro: 31.4
+#Área: 78.5
 
-#Solution 1
+#P=2pi*r
+#A=pi*r²
+#Solution 2
 
-name = input("Enter your name: ")
+import math
 
-print(f"Hi {name}")
+radio = int(input("Enter the radio: "))
+perimeter = round(2 * math.pi * radio, 1)
+area =  round(math.pi * math.pow(radio, 2), 1)
+
+print(f"""
+Perimeter: {perimeter}
+Area: {area}""")
